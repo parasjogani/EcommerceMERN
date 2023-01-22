bdc6d701a030c116980e527bdc07a5abe53fcd4d
parasjogani <paras.jogani@gmail.com>
2023-01-22 10:52:58 +0530

    add user model

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles"
 
 const userSchema = mongoose.Schema(
     {
@@ -6,7 +7,31 @@ const userSchema = mongoose.Schema(
             type: String,
             required: [true, "Name must required"],
             maxLength: [50, "Name must be less than 50 character"]
-            
-        }
+        },
+        email: {
+            type: String,
+            required: [true, "Email must required"],
+            unique: true,
+            match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        },
+        password: {
+            type: String,
+            required: [true, "Password must required"],
+            minLength: [8, "password must be at least 8 characters"],
+            match: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, //checks at least one char, one number and one special char.
+            select: false
+        },
+        role: {
+            type: String,
+            enum: Object.values(AuthRoles),
+            default: AuthRoles.USER
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry: Date,
+    },
+    {
+        timestamps: true
     }
-)
+);
+
+export default mongoose.model("User", userSchema)
