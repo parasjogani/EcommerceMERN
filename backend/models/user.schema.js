f0a2bc99ee5044d77142f57142f42fd16b5d43b3
parasjogani <paras.jogani@gmail.com>
2023-01-22 10:29:17 +0530

    start ecommerce backend part

---
+++ backend/models/user.schema.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Name must required"],
+            maxLength: [50, "Name must be less than 50 character"]
+            
+        }
+    }
+)
