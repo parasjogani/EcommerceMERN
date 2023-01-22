d03865d3b97591b4578fa16971b16c0e8a32f5b4
parasjogani <paras.jogani@gmail.com>
2023-01-22 11:36:03 +0530

    add method in user schema for token and compare password

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -3,6 +3,7 @@ import AuthRoles from "../utils/authRoles"
 import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
 import crypto from "crypto";
+import config from "../config/index";
 
 const userSchema = mongoose.Schema(
     {
@@ -43,5 +44,27 @@ userSchema.pre("save", async function(next){
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
+
+// add more features to schema
+userSchema.methods = {
+    // compare password
+    comparePassword: async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword, this.password)
+    },
+
+    // generate JWT token
+    getJwtToken: function(){
+        return JWT.sign(
+            {
+                _id: this._id,
+                role: this.role
+            },
+            config.JWT_SECRET,
+            {
+                expiresIn: config.JWT_EXPIRY
+            }
+        )
+    }
+}
 
 export default mongoose.model("User", userSchema)
