34d8372bf44146a1b36f2cf4aa1526248a7b3e1d
parasjogani <paras.jogani@gmail.com>
2023-01-22 11:07:15 +0530

    hook for userschema

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles"
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = mongoose.Schema(
     {
@@ -33,5 +36,12 @@ const userSchema = mongoose.Schema(
         timestamps: true
     }
 );
+
+// encrypt password
+userSchema.pre("save", async function(next){
+    if (!this.modified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
 
 export default mongoose.model("User", userSchema)
