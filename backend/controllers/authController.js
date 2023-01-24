e5c5a865110780a024bbb327cd35c2619a75896e
parasjogani <paras.jogani@gmail.com>
2023-01-24 21:12:29 +0530

    authController - forgotpassword

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/user.schema"
 import asyncHandler from "../services/asyncHandler"
 import CustomError from "../utils/customError"
+import mailHelper from "../utils/mailHelper"
 
 
 export const cookieOption = {
@@ -101,4 +102,52 @@ export const logout = asyncHandler(async (req, res) => {
         success: true,
         message: "Logout successfully"
     })
+})
+
+/**************************************************
+ * @FORGOT_PASSWORD
+ * @route http://localhost:4000/api/auth/password/forgot
+ * @description User will submit email and we will generate a token
+ * @parameters email
+ * @returns success message - email - send
+ **************************************************/
+
+export const forgotPassword = asyncHandler(async(req, res) => {
+    const {email} = req.body
+    if (email == "") {
+        throw new CustomError("Please enter valid email", 400)
+    }
+    const user = await User.findOne({email})
+
+    if (!user) {
+        throw new CustomError("User not found", 404)
+    }
+    const resetToken = user.generateForgotPasswordToken()
+
+    await user.save({validateBeforeSave: false})
+
+    const resetUrl = 
+    `${req.protocol}://${req.get("host")}/api/auth/password/reset/${resetToken}`
+
+    const text = `Tap the below link to reset your password
+    \n\n ${resetUrl}\n\n
+    `
+    try {
+        await mailHelper({
+            email: user.email,
+            subject: "Password reset mail",
+            text: text
+        })
+        res.status(200).json({
+            success: true,
+            message: `Email send to ${user.email}`
+        })
+    } catch (error) {
+        user.forgotPasswordToken = undefined
+        user.forgotPasswordExpiry = undefined
+
+        await user.save({validateBeforeSave: false})
+
+        throw new CustomError(error.message || "Something went wrong", 500) 
+    }
 })
