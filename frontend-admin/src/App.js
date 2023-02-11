63536e9d99330bd56af163f83499ba52e4b71c0d
parasjogani <paras.jogani@gmail.com>
2023-02-11 10:24:42 +0530

    Setup MainLayout

--- frontend-admin/src/App.js
+++ frontend-admin/src/App.js
@@ -1,11 +1,24 @@
-
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import MainLayout from './components/MainLayout';
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login';
+import Resetpassword from './pages/Resetpassword';
+import Forgotpassword from './pages/Forgotpassword';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>React app started</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/reset-password' element={<Resetpassword />} />
+        <Route path='/forgot-password' element={<Forgotpassword />} />
+        <Route path='/admin' element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
