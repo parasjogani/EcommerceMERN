62fa285e7604fe8cd5afdbac543b734bce5868a9
parasjogani <paras.jogani@gmail.com>
2023-02-25 11:02:46 +0530

    added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './component/Layout';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route>
-
+          <Route path="/" element={<Layout />}>
+            <Route />
           </Route>
         </Routes>
       </BrowserRouter>
