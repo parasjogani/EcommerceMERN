2cf504ee91483934d3714e29b89fb139fa764cb1
parasjogani <paras.jogani@gmail.com>
2023-02-25 12:11:36 +0530

    added login, signup and forgotpassword page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './component/Layout';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About'
+import Contact from './pages/Contact';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Forgotpassword from './pages/Forgotpassword'
 
 function App() {
   return (
@@ -9,7 +15,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route />
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path='signup' element={<Signup />} />
+            <Route path='login' element={<Login />} />
+            <Route path='forgot-password' element={<Forgotpassword />} />
           </Route>
         </Routes>
       </BrowserRouter>
